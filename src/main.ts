2400712ad123b30e4290c450e9605f51597041ae
Epokuiso <epokuiso@gmail.com>
2022-11-05 10:58:35 +0100

    setting up body validation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from "@nestjs/core";
+import { ValidationPipe } from "@nestjs/common";
 import { AppModule } from "./app.module";
 
 const bootstrap = async () =>
 {
     const app = await NestFactory.create (AppModule);
+    app.useGlobalPipes (
+        new ValidationPipe ({
+            whitelist: true
+    }));
     app.listen (3000);
 }
 
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { 
+    Controller, 
+    Get, 
+    Post, 
+    Patch, 
+    Delete, 
+    Param, 
+    Body 
+} from '@nestjs/common';
+import { CreateUserDTO } from './dtos/create-user.dto';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController 
+{
+    @Post('/signup')
+    createUser (@Body () userInformation: CreateUserDTO)
+    {
+        console.log(userInformation);
+    }
+
+    @Get('/:id')
+    findUser (@Param() id: string)
+    {
+        console.log (`Here's the id: ${id}.`);
+        return "Hello World";
+    }
+}
