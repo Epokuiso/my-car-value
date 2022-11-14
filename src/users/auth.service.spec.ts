72aa8fd58d06fd835212de1b4bc11b4f73869eb9
Epokuiso <epokuiso@gmail.com>
2022-11-14 10:24:19 +0100

    Improving File Layout

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -3,26 +3,34 @@ import { AuthService } from "./auth.service";
 import { User } from "./user.entity";
 import { UsersService} from "./users.service";
 
-it ('can create an instance of auth service', async () => 
+describe ('AuthService', () =>
 {
-    const fakeUsersService: Partial<UsersService> = {
-        find: () => Promise.resolve ([]),
-        findByEmail: (email: string) => Promise.resolve ([]),
-        create: (email: string, password: string) => 
-            Promise.resolve ({ id: 1, email, password } as User)
- 
-    }
+    let service: AuthService;
 
-    const module = await Test.createTestingModule ({
-        providers: [AuthService,
-        {
-            provide: UsersService,
-            useValue: fakeUsersService
-        }],
-        
-    }).compile ();
+    beforeEach (async () => 
+    {
+        const fakeUsersService: Partial<UsersService> = {
+            find: () => Promise.resolve ([]),
+            findByEmail: (email: string) => Promise.resolve ([]),
+            create: (email: string, password: string) => 
+                Promise.resolve ({ id: 1, email, password } as User)
+        }
 
-    const service = module.get (AuthService);
+        const module = await Test.createTestingModule ({
+            providers: [AuthService,
+            {
+                provide: UsersService,
+                useValue: fakeUsersService
+            }],
+            
+        }).compile ();
 
-    expect (service).toBeDefined ();    
-})
+        service = module.get (AuthService);
+    });
+
+
+    it ('can create an instance of auth service', async () => 
+    {
+        expect (service).toBeDefined ();    
+    });
+});
