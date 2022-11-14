d46b5bea90d9b664a7c8e329c6c26193c07407e9
Epokuiso <epokuiso@gmail.com>
2022-11-14 14:37:01 +0100

    unit testing a controller

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,18 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
+import { User } from './user.entity';
 
-describe('UsersController', () => {
+describe('UsersController', () => 
+{
   let controller: UsersController;
+  let fakeAuthService: Partial<AuthService>;
+  let fakeUsersService: Partial<UsersService>;
+  
 
-  beforeEach(async () => {
+  beforeEach(async () => 
+  {
+    fakeAuthService = {
+      signup: (email: string, password: string) => Promise.resolve ({} as User),
+      signin: (email: string, password: string) => Promise.resolve ({} as User)
+    };
+    fakeUsersService = {
+      findByEmail: (email: string) => Promise.resolve ([]),
+      find: () => Promise.resolve ([]),
+      findOne: (userId: number) => Promise.resolve ({} as User),
+      update: (id: number, userProperties: User) => Promise.resolve ({} as User),
+      remove: (id: number) => Promise.resolve ({} as User),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-    }).compile();
+      providers: [AuthService]
+    }).compile ();
 
-    controller = module.get<UsersController>(UsersController);
+    controller = module.get<UsersController> (UsersController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should be defined', () => 
+  {
+    expect (controller).toBeDefined ();
   });
 });
